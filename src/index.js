5eb4eed51d4b71b8c8476cdb43902ff253ec656a
Daniel Saavedra <danielink28@gmail.com>
2021-11-25 07:34:45 -0500

    First use of mutation in react

--- src/index.js
+++ src/index.js
@@ -5,43 +5,20 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  useQuery,
-  gql
 } from "@apollo/client";
+import Proyectos from './components/Proyectos';
+import CrearProyecto from './components/CrearProyecto';
 
 const client = new ApolloClient({
   uri: 'http://localhost:9092/graphql',
   cache: new InMemoryCache()
 });
 
-const Proyectos = () => {
-  const PROYECTOS = gql`
-  query  {
-    proyectos {
-      lider
-      facultad
-    }
-  }
-`;
-  const { loading, error, data } = useQuery(PROYECTOS)
-  if (loading) return "<h1>Cargando</h1>"
-  console.log(data)
-
-  const datosTabla = data.proyectos.map(({ lider, facultad }) => (
-    <tr>
-      <td>{lider}</td>
-      <td>{facultad}</td>
-    </tr>
-  ));
-
-  return <table>{datosTabla}</table>
-
-
-}
 
 
 const inicio = document.getElementById("root")
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Proyectos />
+    <CrearProyecto />
   </ApolloProvider>, inicio)
